53afab6f390a7170348d8c99af65d0e30fd3740c
az <16586146+ammarzia@users.noreply.github.com>
2019-02-21 16:45:22 -0500

    refactor

--- js/main.js
+++ js/main.js
@@ -1,216 +1,159 @@
 (function() {
-    function coin(id, price, oldPrice, marketCap, oldMarketCap, volume, oldVolume, marketShare, oldMarketShare, change, oldChange) {
-    	this.id = id;
-        this.price = price;
-        this.oldPrice = oldPrice;
-        this.marketCap = marketCap;
-        this.oldMarketCap = oldMarketCap;
-        this.volume = volume;
-        this.oldVolume = oldVolume;
-        this.marketShare = marketShare;
-        this.oldMarketShare = oldMarketShare;
-        this.change = change;
-        this.oldChange = oldChange;
+
+    let isFirstLoad = true;
+
+    class Coin {
+        constructor(id) {
+            this.id = id;
+            this.price = 0;
+            this.oldPrice = 0;
+            this.marketCap = 0;
+            this.oldMarketCap = 0;
+            this.volume = 0;
+            this.oldVolume = 0;
+            this.marketShare = 0;
+            this.oldMarketShare = 0;
+            this.change = 0;
+            this.oldChange = 0;
+        }
     }
 
-    var bitcoin = new coin("bitcoin");
-    var bitcoinCash = new coin("bitcoin-cash");
-    var ethereum = new coin("ethereum");
-    var monero = new coin("monero");
-    var siacoin = new coin("siacoin");
+    const coins = ["bitcoin", "bitcoin-cash", "ethereum", "monero", "siacoin"].map(id => new Coin(id));
+    let totalMarketCap, oldTotalMarketCap;
+    let chart;
 
-    var coinList = [bitcoin, bitcoinCash, ethereum, monero, siacoin];
-
-    var totalMarketCap, oldTotalMarketCap;
-
-    var chart;
-
-    $(document).ready(function() {
-        myTimer();
-        setInterval(function() {
-            myTimer();
-        }, 60000);
+    $(document).ready(() => {
+        updateData();
+        setInterval(updateData, 60000);
         createChart();
     });
 
-    function myTimer() {
-
-        function runInOrder(callback) {
-            getGlobalData(function() {
-                getTickerData(function() {
-                    updateTable(callback);
-                });
-            });
-        };
-
-        runInOrder();
-
-        function getGlobalData(callback) {
-            $.getJSON("https://api.coingecko.com/api/v3/global", function(json) {
-				totalMarketCap = fixed(parseFloat(json.data.total_market_cap.usd / 1000000000), 2);
-				callback();
-			});
-        }
-
-        function getTickerData(callback) {
-        	$.getJSON('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false', function(json) {
-        	    for (var i = 0; i < coinList.length; i++) {
-        	    $.each(json, function(index, value) {
-        	    	if (value.id == coinList[i].id) {
-        	    		coinList[i].marketCap = parseInt(value.market_cap);
-        	    		coinList[i].price = fixed(parseFloat(value.current_price), 2);
-        	    		coinList[i].volume = parseInt(value['total_volume']);
-                        coinList[i].change = fixed(parseFloat(value.price_change_percentage_24h), 2);
-                        coinList[i].marketShare = fixed((coinList[i].marketCap * 0.0000001 / totalMarketCap), 2);
-        	    	}
-        	    });
-                }
-			callback();
-            });	
-        }
-
-        function updateTable(callback) {
-
-            // Update total market cap.
-            if (!oldTotalMarketCap || oldTotalMarketCap == totalMarketCap) {
-                $('#totalMarketCap').text('$' + totalMarketCap + 'B').removeClass();
-            } else if (totalMarketCap > oldTotalMarketCap) {
-                $('#totalMarketCap').text('$' + totalMarketCap + 'B').removeClass().addClass('green');
-            } else {
-                $('#totalMarketCap').text('$' + totalMarketCap + 'B').removeClass().addClass('red');
-            }
-            oldTotalMarketCap = totalMarketCap;
-
-            // Update entries for each coin.
-            for (var i = 0; i < coinList.length; i++) {
-
-                if (!coinList[i].oldPrice || coinList[i].oldPrice == coinList[i].price) {
-                    $('#' + coinList[i].id + 'Price').text('$' + coinList[i].price.toLocaleString().commafy()).removeClass();
-                } else if (coinList[i].price > coinList[i].oldPrice) {
-                    $('#' + coinList[i].id + 'Price').text('$' + coinList[i].price.toLocaleString().commafy()).removeClass().addClass('green');
-                } else {
-                    $('#' + coinList[i].id + 'Price').text('$' + coinList[i].price.toLocaleString().commafy()).removeClass().addClass('red');
-                }
-                coinList[i].oldPrice = parseFloat(coinList[i].price);
-
-                if (!coinList[i].oldMarketCap || coinList[i].oldMarketCap == coinList[i].marketCap) {
-                    $('#' + coinList[i].id + 'MarketCap').text('$' + coinList[i].marketCap.toLocaleString()).removeClass();
-                } else if (coinList[i].marketCap > coinList[i].oldMarketCap) {
-                    $('#' + coinList[i].id + 'MarketCap').text('$' + coinList[i].marketCap.toLocaleString()).removeClass().addClass('green');
-                } else {
-                    $('#' + coinList[i].id + 'MarketCap').text('$' + coinList[i].marketCap.toLocaleString()).removeClass().addClass('red');
-                }
-                coinList[i].oldMarketCap = coinList[i].marketCap;
-
-                if (!coinList[i].oldMarketShare || coinList[i].oldMarketShare == coinList[i].MarketShare) {
-                    $('#' + coinList[i].id + 'MarketShare').text(coinList[i].marketShare + '%').removeClass();
-                    chart.data.datasets[0].data[i] = fixed(coinList[i].marketShare, 2);
-                    chart.update();
-                } else if (coinList[i].marketShare > coinList[i].oldMarketShare) {
-                    $('#' + coinList[i].id + 'MarketShare').text(coinList[i].marketShare + '%').removeClass().addClass('green');
-                    chart.data.datasets[0].data[i] = fixed(coinList[i].marketShare, 2);
-                    chart.update();
-                } else {
-                    $('#' + coinList[i].id + 'MarketShare').text(coinList[i].marketShare + '%').removeClass().addClass('red');
-                    chart.data.datasets[0].data[i] = fixed(coinList[i].marketShare, 2);
-                    chart.update();
-                }
-                coinList[i].oldMarketShare = coinList[i].marketShare;
-
-                if (!coinList[i].oldVolume || coinList[i].oldVolume == coinList[i].volume) {
-                    $('#' + coinList[i].id + 'Volume').text('$' + coinList[i].volume.toLocaleString().commafy()).removeClass();
-                } else if (coinList[i].volume > coinList[i].oldVolume) {
-                    $('#' + coinList[i].id + 'Volume').text('$' + coinList[i].volume.toLocaleString().commafy()).removeClass().addClass('green');
-                } else {
-                    $('#' + coinList[i].id + 'Volume').text('$' + coinList[i].volume.toLocaleString().commafy()).removeClass().addClass('red');
-
-                }
-                coinList[i].oldVolume = parseInt(coinList[i].volume);
-
-                if (!coinList[i].oldChange || coinList[i].oldChange == coinList[i].change) {
-                    $('#' + coinList[i].id + 'Change').text(coinList[i].change.commafy() + '%').removeClass();
-                } else if (parseFloat(coinList[i].change) > parseFloat(coinList[i].oldChange)) {
-                    $('#' + coinList[i].id + 'Change').text(coinList[i].change.commafy() + '%').removeClass().addClass('green');
-                } else {
-                    $('#' + coinList[i].id + 'Change').text(coinList[i].change.commafy() + '%').removeClass().addClass('red');
-                }
-                coinList[i].oldChange = coinList[i].change;
-            }
-
-            chart.data.datasets[0].data[5] = fixed(calculateOthersMarketShare(), 2);
-            chart.update();
-
-            updateBrowserTab();
-
-            callback && callback();
+    async function updateData() {
+        try {
+            await getGlobalData();
+            await getTickerData();
+            updateTable();
+            if (isFirstLoad) isFirstLoad = false;
+        } catch (error) {
+            console.error("Error updating data:", error);
         }
     }
 
+    async function getGlobalData() {
+        try {
+            const response = await fetch("https://api.coingecko.com/api/v3/global");
+            const json = await response.json();
+            if (!isFirstLoad) {
+                oldTotalMarketCap = totalMarketCap
+            }
+            totalMarketCap = fixed(parseFloat(json.data.total_market_cap.usd / 1000000000), 2);
+            if (isFirstLoad) {
+                oldTotalMarketCap = totalMarketCap
+            }
+        } catch (error) {
+            console.error("Error fetching global data:", error);
+        }
+    }
 
-    function createChart() {
-
-        $(function() {
-
-            Chart.defaults.global.legend.display = false;
-            Chart.defaults.global.animation.duration = 0;
-            Chart.defaults.global.defaultFontFamily = "Verdana";
-
-            var ctx = document.getElementById('myChart').getContext('2d');
-
-            chart = new Chart(ctx, {
-
-                // The type of chart we want to create
-                type: 'horizontalBar',
-
-                // The data for our dataset
-                data: {
-                    labels: ["Bitcoin", "Bitcoin Cash", "Ethereum", "Monero", "Siacoin", "Others"],
-                    datasets: [{
-                        backgroundColor: ['#2196f3', '#f44336', '#9c27b0', '#ffca28', '#439647', '#ffffff'],
-                        borderColor: '#000000',
-                    }]
-                },
-
-                // Configuration options go here
-                options: {
-                    scales: {
-                        yAxes: [{
-                            id: 'y-axis-0',
-                            gridLines: {
-                                display: false,
-                            },
-                            ticks: {
-                                fontColor: "white",
-                                fontSize: 14,
-                                beginAtZero: true,
-                            },
-                            afterBuildTicks: function(chart) {}
-                        }],
-                        xAxes: [{
-                            id: 'x-axis-0',
-                            gridLines: {
-                                display: false
-                            },
-                            ticks: {
-                                fontColor: "white",
-                                fontSize: 14,
-                                beginAtZero: true,
-                            }
-                        }]
-                    },
-                    tooltips: {
-                        enabled: true,
-                        mode: 'single',
-                        callbacks: {
-                            label: function(tooltipItem, data) {
-                                var dataset = data['datasets'][0];
-                                var percent = dataset['data'][tooltipItem['index']];
-                                return percent + '%';
-                            }
-                        }
+    async function getTickerData() {
+        try {
+            const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false');
+            const data = await response.json();
+            coins.forEach(coin => {
+                const coinData = data.find(item => item.id === coin.id);
+                if (coinData) {
+                    if (!isFirstLoad) {
+                        coin.oldMarketCap = coin.marketCap;
+                        coin.oldPrice = coin.price;
+                        coin.oldVolume = coin.volume;
+                        coin.oldChange = coin.change;
+                        coin.oldMarketShare = coin.marketShare;
+                    }
+                    coin.marketCap = parseInt(coinData.market_cap);
+                    coin.price = fixed(parseFloat(coinData.current_price), 2);
+                    coin.volume = parseInt(coinData.total_volume);
+                    coin.change = fixed(parseFloat(coinData.price_change_percentage_24h), 2);
+                    coin.marketShare = fixed((coin.marketCap * 0.0000001 / totalMarketCap), 2);
+                    if (isFirstLoad) {
+                        coin.oldMarketCap = coin.marketCap;
+                        coin.oldPrice = coin.price;
+                        coin.oldVolume = coin.volume;
+                        coin.oldChange = coin.change;
+                        coin.oldMarketShare = coin.marketShare;
                     }
                 }
             });
+        } catch (error) {
+            console.error("Error fetching ticker data:", error);
+        }
+    }
+
+    function updateTable() {
+        updateTotalMarketCap();
+        coins.forEach((coin, i) => {
+            updateCoinData(coin, 'Price', coin.price, coin.oldPrice, '$', '');
+            updateCoinData(coin, 'MarketCap', coin.marketCap, coin.oldMarketCap, '$', '');
+            updateCoinData(coin, 'Volume', coin.volume, coin.oldVolume, '$', '');
+            updateCoinData(coin, 'MarketShare', coin.marketShare, coin.oldMarketShare, '', '%');
+            updateCoinData(coin, 'Change', coin.change, coin.oldChange, '', '%');
+            chart.data.datasets[0].data[i] = fixed(coin.marketShare, 2);
+        });
+        chart.data.datasets[0].data[5] = fixed(calculateOthersMarketShare(), 2);
+        chart.update();
+        updateBrowserTab();
+    }
+
+    function updateTotalMarketCap() {
+        const element = $('#totalMarketCap');
+        const newText = `$${totalMarketCap}B`;
+        if (element.text() !== newText) {
+            element.text(newText).removeClass();
+            if (oldTotalMarketCap !== undefined && oldTotalMarketCap !== totalMarketCap) {
+                element.addClass(totalMarketCap > oldTotalMarketCap ? 'green' : 'red');
+            }
+        }
+    }
+
+    function updateCoinData(coin, field, newValue, oldValue, prefix = '', suffix = '') {
+        const element = $(`#${coin.id}${field}`);
+        const newText = `${prefix}${newValue.toLocaleString().commafy()}${suffix}`;
+        if (element.text() !== newText) {
+            element.text(newText).removeClass();
+            if (oldValue !== undefined && oldValue !== newValue) {
+                element.addClass(newValue > oldValue ? 'green' : 'red');
+            }
+        }
+    }
+
+    function createChart() {
+        const ctx = document.getElementById('myChart').getContext('2d');
+        Chart.defaults.global.animation.duration = 0;
+        Chart.defaults.global.defaultFontFamily = "Verdana";
+        chart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: {
+                labels: ["Bitcoin", "Bitcoin Cash", "Ethereum", "Monero", "Siacoin", "Others"],
+                datasets: [{
+                    backgroundColor: ['#2196f3', '#f44336', '#9c27b0', '#ffca28', '#439647', '#ffffff'],
+                    borderColor: '#000000',
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{ gridLines: { display: false }, ticks: { fontColor: "white", fontSize: 14, beginAtZero: true } }],
+                    xAxes: [{ gridLines: { display: false }, ticks: { fontColor: "white", fontSize: 14, beginAtZero: true } }]
+                },
+                tooltips: {
+                    enabled: true,
+                    mode: 'single',
+                    callbacks: {
+                        label: (tooltipItem, data) => `${data.datasets[0].data[tooltipItem.index]}%`
+                    }
+                },
+                legend: {
+                    display: false
+                }
+            }
         });
     }
 
@@ -226,17 +169,18 @@
     // Formats string representation of numbers with commas. 
     // E.g. 1000 --> 1,000
     String.prototype.commafy = function() {
-        return this.replace(/(^|[^\w.])(\d{4,})/g, function($0, $1, $2) {
-            return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, '$&,');
-        });
+        return this.replace(/(^|[^\w.])(\d{4,})/g, ($0, $1, $2) => $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, '$&,'));
     };
 
     function updateBrowserTab() {
-        document.title = "Crypto Compare | $" + bitcoin.price.toLocaleString().commafy();
+        const bitcoinPrice = coins.find(coin => coin.id === 'bitcoin')?.price;
+        if (bitcoinPrice) {
+            document.title = `Crypto Compare | $${bitcoinPrice.toLocaleString().commafy()}`;
+        }
     }
 
     function calculateOthersMarketShare() {
-        return 100 - bitcoin.marketShare - bitcoinCash.marketShare - ethereum.marketShare - monero.marketShare - siacoin.marketShare;
+        return 100 - coins.reduce((acc, coin) => acc + parseFloat(coin.marketShare), 0);
     }
 
 })();
